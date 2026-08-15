28b9a8196f904b8272b6b42f9f0a1988bfcefe5b
PedroAP14 <pamericoparussolo@gmail.com>
2026-08-14 22:19:24 -0300

    Notes v1

---
+++ Notes/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿namespace Notes
+{
+    public partial class MainPage : ContentPage
+    {
+        string caminho = Path.Combine(FileSystem.AppDataDirectory, "nota.txt");
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AoClicarlNoSalvar(object? sender, EventArgs e)
+        {
+            string conteudo = EditorAnotacao.Text;
+            File.WriteAllText(caminho, conteudo);
+        }
+
+        private void AoClicarlNoApagar(object? sender, EventArgs e)
+        {
+
+        }
+
+        private void AoClicarlNoCarregar(object? sender, EventArgs e)
+        {
+
+        }
+    }
+}
